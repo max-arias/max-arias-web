484f14da3c29534af613e1ca47ef94675f152cbc
Max Arias <max@max-arias.com>
2019-12-07 20:29:29 -0300

    Initial work

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<style lang="postcss">
+	main {
+		color: red;
+	}
+	h1 {
+		font-family: 'Roboto Slab', serif;
+		font-size: 32px;
+		color: red;
+	}
+
+	p {
+		font-family: 'Roboto Slab', serif;
+	}
+</style>
+
+<main class="flex mb-4">
+	<h1>Hey! I'm Max</h1>
+	<p>I'm a full-stack web developer. How can I help?</p>
+</main>
